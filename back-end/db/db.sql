e3330a95ab6b974e29d9804748b91dafb1d84a50
Lucas Rissi <rissi469@gmail.com>
2020-10-15 17:57:01 -0300

    Script para ser executado na database

---
+++ back-end/db/db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE `ciapretro`;
+
+USE DATABASE `ciapretro`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` VARCHAR(60) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `register_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `unq_idx_by_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(60) DEFAULT NULL,
+  `source_coin` varchar(3) DEFAULT NULL,
+  `destiny_coin` varchar(3) DEFAULT NULL,
+  `value_to_convert` double DEFAULT NULL,
+  `conversion_value` double DEFAULT NULL,
+  `create_at` DATE NOT NULL CURRENT_DATE,
+  PRIMARY KEY (`id`),
+  INDEX `idx_by_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
